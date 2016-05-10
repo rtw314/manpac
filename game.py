897b6334fac6c7be825572cbec69a831f8e2ca4e
Ronald Waite <waiteforme15@gmail.com>
2016-05-10 22:11:06 +0900

    Move game into game.py, reduce window size to 300x300

--- game.py
+++ game.py
@@ -1,31 +1,24 @@
+import os, sys
 import pygame
-from pygame.locals import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption('Basic Pygame program')
-
+    maze = pygame.image.load(os.path.join('maps', 'maze1.png'))
+    screen = pygame.display.set_mode((300, 300))
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((250, 250, 250))
-
-    font = pygame.font.Font(None, 36)
-    text = font.render("Hello there", 1, (10, 10, 10))
-    textpos = text.get_rect()
-    textpos.centerx = background.get_rect().centerx
-    textpos.centery = background.get_rect().centery / 2
-    background.blit(text, textpos)
-
+    for y in range(30):
+        for x in range(30):
+            background.fill(maze.get_at((x, y)), pygame.Rect(x * 10, y * 10, x * 10 + 9, y * 10 + 9))
+    #background.blit(maze, (150, 150))
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
     while 1:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == pygame.QUIT:
                 return
-
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
 if __name__ == '__main__': main()
