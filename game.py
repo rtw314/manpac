0bfa0d63ea586b60d913680e44f8c2d12d09eb23
Ronald Waite <waiteforme15@gmail.com>
2016-05-10 22:27:37 +0900

    Add the player sprite to the map start location

--- game.py
+++ game.py
@@ -7,9 +7,15 @@ def main():
     screen = pygame.display.set_mode((300, 300))
     background = pygame.Surface(screen.get_size())
     background = background.convert()
+    player = pygame.image.load(os.path.join('sprites', 'player_small.png'))
     for y in range(30):
         for x in range(30):
-            background.fill(maze.get_at((x, y)), pygame.Rect(x * 10, y * 10, x * 10 + 9, y * 10 + 9))
+            fill_color = maze.get_at((x, y))
+            current_loc = (x * 10, y * 10)
+            background.fill(fill_color, pygame.Rect(x * 10, y * 10, x * 10 + 9, y * 10 + 9))
+            if fill_color.r == 0:
+                if fill_color.g == 255:
+                    background.blit(player, current_loc)
     #background.blit(maze, (150, 150))
     screen.blit(background, (0, 0))
     pygame.display.flip()
